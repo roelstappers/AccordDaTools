cede947b176e94ac21d215756df88b628c0b3b8a
Roel Stappers <roels@met.no>
2023-04-21 23:51:56 +0200

    Add menu

--- scripts/julia/makieplotinc.jl
+++ scripts/julia/makieplotinc.jl
@@ -1,7 +1,7 @@
 using GLMakie, NCDatasets
 
 
-archive="/home/roels/plots/envar/"
+archive="/home/roel/envar/"
 an_filename="MXMIN1999+0000.nc"
 bg_filename="ICMSHHARM+0003.nc"
 
@@ -13,15 +13,20 @@ ands = NCDataset("$archive/$an_filename")
 
 
 
-fields= ["TEMPERATURE", "HUMI.SPECIFI"]
-fields= ["HUMI.SPECIFI","TEMPERATURE"]
-
+fields= ["TEMPERATURE", "HUMI.SPECIFI","WIND.U.PHYS","WIND.V.PHYS"]
 
 fig = Figure() 
 
 
-s1 = Slider(fig[2,1], range = 1:65, startvalue = 65)
-field = lift( lev -> "S$(lpad(lev,3,"0"))$(fields[1])", s1.value)
+s1 = Slider(fig[1,2], range = 65:-1:1, startvalue = 65,horizontal=false)
+m1 = Menu(fig[2,1], options=fields, default = fields[1])
+
+name = Observable(fields[1])
+on(m1.selection) do x 
+    name[] = x
+    display(fig)
+end 
+field = lift( (lev,m)-> "S$(lpad(lev,3,"0"))$(m)", s1.value,name)
 bgf = lift( f -> bgds[f][:,:] , field) 
 anf = lift( f -> ands[f][:,:] , field) 
 inc = lift( (an,bg) -> an-bg, anf, bgf)
@@ -31,16 +36,54 @@ crange = lift( m -> (-m,m),  maxabsc)
       
 ax = Axis3(
         fig[1,1],
-       	#   protrusions=1,
+      #  	#   protrusions=1,
+       #   aspect = DataAspect(),
           title=title,
           xlabel = "$(crange.val[1])",
           elevation = pi/2, azimuth=-pi/2  ,                       
      ) 
+# ax = Axis(fig[1,1])
 
+
+fields= ["TEMPERATURE", "HUMI.SPECIFI","WIND.U.PHYS","WIND.V.PHYS"]
+
+fig = Figure() 
+
+
+s1 = Slider(fig[1,1], range = 65:-1:1, startvalue = 65,horizontal=false)
+m1 = Menu(fig[2,1:3], options=fields, default = fields[1])
+
+name = Observable(fields[1])
+on(m1.selection) do x 
+    name[] = x
+    display(fig)
+end 
+field = lift( (lev,m)-> "S$(lpad(lev,3,"0"))$(m)", s1.value,name)
+bgf = lift( f -> bgds[f][:,:] , field) 
+anf = lift( f -> ands[f][:,:] , field) 
+inc = lift( (an,bg) -> an-bg, anf, bgf)
+maxabsc = lift( x -> maximum(abs.(x)),inc)
+title = lift( (f,mac)  -> "Hi $(f) colormax $mac" , field, maxabsc)  
+crange = lift( m -> (-m,m),  maxabsc) 
+      
+ax = Axis3(
+        fig[1,2],
+      #  	#   protrusions=1,
+       #   aspect = DataAspect(),
+          viewmode  = :stretch,
+          title=title,
+          xlabel = "$(crange.val[1])",
+          elevation = pi/2, azimuth=-pi/2  ,                       
+     ) 
+# ax = Axis(fig[1,1])
 hidedecorations!(ax)
 hidespines!(ax)
-surface!(ax,bgf,
+surf = surface!(ax,bgf,
     color=inc,
     colormap=Reverse(:RdBu),
-    colorrange=crange
+    colorrange=crange,
+    shading=true
 )
+Colorbar(fig[1,3],surf,ticklabelspace=30)
+
+fig
