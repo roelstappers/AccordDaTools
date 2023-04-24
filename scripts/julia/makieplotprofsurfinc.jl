0206f39333af21d76e294a41c0928ced2c8c2dc6
Roel Stappers <roels@met.no>
2023-04-24 13:41:58 +0200

    update makieplotprofsurfinc.jl

--- scripts/julia/makieplotprofsurfinc.jl
+++ scripts/julia/makieplotprofsurfinc.jl
@@ -1,11 +1,18 @@
-using GLMakie, NCDatasets
+using GLMakie, NCDatasets, Statistics
 
-archive="/home/roel/envar/"
-an_filename="MXMIN1999+0000.nc"
+archive="/home/roels/plots/"
+
+an_filename="MXMIN1999+0000.nc"   # Needs /YYYY/MN/DD/HH
 bg_filename="ICMSHHARM+0003.nc"
 
-bgds = NCDataset("$archive/$bg_filename");
-ands = NCDataset("$archive/$an_filename")
+expenvar = "envar"
+bgds1 = NCDataset(joinpath(archive,expenvar,bg_filename));
+ands1 = NCDataset(joinpath(archive,expenvar,an_filename));
+
+exp3dvar = "3dvar"
+bgds2 = NCDataset(joinpath(archive,exp3dvar,bg_filename));
+ands2 = NCDataset(joinpath(archive,exp3dvar,an_filename));
+
 
 #bgds = NCDataset(ARGS[1])
 #ands = NCDataset(ARGS[2])
@@ -56,7 +63,7 @@ function makeprofplot(fig,bgds,ands,var,lev::Observable)
     for lev in 1:nlev
         field = "S$(lpad(lev,3,"0"))$var"
         inc = ands[field][:,:] - bgds[field][:,:]
-        val[lev]= sum(inc.^2)
+        val[lev]= sqrt(mean(inc.^2))
     end
     lines!(ax, val, 1:65, color=:red)
     pval = lift( lev -> val[lev], lev)
@@ -77,13 +84,19 @@ set_theme!(theme_dark())
 fig = Figure() 
 s1 = Slider(fig[1:1,1], range = 65:-1:1, startvalue = 65,horizontal=false)
 
-a1 = makesurfplot(fig[1,2],bgds,ands,fields[1],s1.value,surfbg=false ,xrange=xrange, yrange=yrange)
+
+
+a1 = makesurfplot(fig[1,2],bgds1,ands1,fields[1],s1.value,surfbg=true ,xrange=xrange, yrange=yrange)
+a1 = makesurfplot(fig[1,3],bgds2,ands2,fields[1],s1.value,surfbg=true ,xrange=xrange, yrange=yrange)
+
 a2 = makesurfplot(fig[1,3],bgds,ands,fields[2],s1.value,surfbg=true ,xrange=xrange, yrange=yrange)
 a3 = makesurfplot(fig[2,2],bgds,ands,fields[3],s1.value,surfbg=true)
 a4 = makesurfplot(fig[2,3],bgds,ands,fields[4],s1.value,surfbg=true)
 fig
 
-px1 = makeprofplot(fig[1,2,Right()],bgds,ands,fields[1],s1.value)
+px1 = makeprofplot(fig[1,2,Right()],bgds1,ands1,fields[1],s1.value)
+px1 = makeprofplot(fig[1,3,Right()],bgds2,ands2,fields[1],s1.value)
+
 px2 = makeprofplot(fig[1,3,Right()],bgds,ands,fields[2],s1.value)
 px3 = makeprofplot(fig[2,2,Right()],bgds,ands,fields[3],s1.value)
 px4 = makeprofplot(fig[2,3,Right()],bgds,ands,fields[4],s1.value)
